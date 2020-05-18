8560fb557be03e6af187ddd97a32cb6dec121d66
Nicolas Choquet <nicolachoquet06250@gmail.com>
2020-05-18 13:13:56 +0200

    ajouts de composants ajouts de plusieurs batiements à la map

--- js/components/seasons.js
+++ js/components/seasons.js
@@ -4,18 +4,28 @@ AFRAME.registerComponent('seasons', {
 	},
 	init() {
 		if (this.data.active) {
-			let currentYear =  (new Date()).getFullYear();
-			fetch(`https://kalendrier.ouest-france.fr/dates-changements-saisons-${currentYear}.html`,
-				{
-					headers: {
-						Host: 'kalendrier.ouest-france.fr'
-					}
-				}).then(r => r.text())
-				.then(html => {
-					let parser = new DOMParser();
-					let dom = parser.parseFromString(html, 'text/html');
-					console.log(dom.querySelector('table#saisons'));
-				});
+			// let currentYear =  (new Date()).getFullYear();
+			// fetch(`https://kalendrier.ouest-france.fr/dates-changements-saisons-${currentYear}.html`,
+			// 	{
+			// 			headers: {
+			// 				Host: 'kalendrier.ouest-france.fr',
+			// 				Referer: `https://kalendrier.ouest-france.fr/dates-changements-saisons-${currentYear}.html`,
+			// 				Origin: `https://kalendrier.ouest-france.fr/dates-changements-saisons-${currentYear}.html`,
+			// 				'Access-Control-Allow-Origin': '*'
+			// 			}
+			// 		}).then(r => r.text())
+			// 	.then(html => {
+			// 		let parser = new DOMParser();
+			// 		let dom = parser.parseFromString(html, 'text/html');
+			// 		console.log(dom.querySelector('table#saisons'));
+			// 	});
+
+			fetch('https://en.wikipedia.org/w/api.php?action=parse&page=Saison&format=json').then(r => r.json()).then(json => {
+				let text = json.parse.text;
+				let parser = new DOMParser();
+				let dom = parser.parseFromString(text, 'text/html');
+				console.log(dom.querySelector('.wikitable.right'));
+			});
 		}
 	}
 });
