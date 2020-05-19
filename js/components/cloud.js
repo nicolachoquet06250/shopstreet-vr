0853c24ebfd0d2074a08aca16cad4ecfd78dd0b0
Nicolas Choquet <nicolachoquet06250@gmail.com>
2020-05-19 11:28:21 +0200

    ajout d'une vrai pluie avec plusieurs "goutes" avec des positions de départ aléatoires

--- js/components/cloud.js
+++ js/components/cloud.js
@@ -23,25 +23,143 @@ AFRAME.registerComponent('cloud', {
 		return sphere;
 	},
 	createRain() {
+		let cloudPositionY = this.el.getAttribute('position').y;
+
 		let goutte = document.createElement('a-box');
+		let x = Math.floor(Math.random() * (3 - (-3) + 1)) + (-3);
+		let z = Math.floor(Math.random() * (4 - (-4) + 1)) + (-4);
+
+		x = 0;
+		z = 0;
+
 		goutte.setAttribute('width', 1);
 		goutte.setAttribute('height', 1);
 		goutte.setAttribute('depth', 1);
 		goutte.setAttribute('color', 'blue');
 		goutte.setAttribute('static-body', '');
-		let x = Math.floor(Math.random() * (3 - (-3) + 1)) + (-3);
-		let z = Math.floor(Math.random() * (4 - (-4) + 1)) + (-4);
 		goutte.setAttribute('position', { x, y: -1, z });
-		let interval = setInterval(() => {
+
+		goutte.addEventListener('collide', e => {
+			console.log('la goute d\'eau à touché quelque chose !!');
+		})
+
+		goutte.interval = setInterval(() => {
 			let new_position = goutte.getAttribute('position');
 			new_position.y -= 0.2;
 			new_position.x -= 0.1;
-			if (new_position.y <= -99) {
-				clearInterval(interval);
+			if (new_position.y <= -(cloudPositionY - 1)) {
+				clearInterval(goutte.interval);
+				// let position1 = goutte.getAttribute('position');
+				// let position2 = goutte.getAttribute('position');
+				// let position3 = goutte.getAttribute('position');
+				// let position4 = goutte.getAttribute('position');
 				this.el.removeChild(goutte);
+
+				// let eclaboussure1 = document.createElement('a-box');
+				// eclaboussure1.setAttribute('width', 1);
+				// eclaboussure1.setAttribute('height', 1);
+				// eclaboussure1.setAttribute('depth', 1);
+				// eclaboussure1.setAttribute('position', position1);
+				// this.el.appendChild(eclaboussure1);
+				// eclaboussure1.cmp = 0;
+				// eclaboussure1.interval = setInterval(() => {
+				// 	console.log('avant', position1);
+				// 	position1.x -= 0.1;
+				// 	if (eclaboussure1.cmp <= 10) {
+				// 		position1.y += 0.1;
+				// 	} else {
+				// 		position1.y -= 0.1;
+				// 	}
+				// 	position1.z += 0.1;
+				// 	console.log('apres', position1);
+				//
+				// 	eclaboussure1.setAttribute('position', position1);
+				//
+				// 	if (eclaboussure1.cmp === 20) {
+				// 		clearInterval(eclaboussure1.interval);
+				// 		this.el.removeChild(eclaboussure1);
+				// 	}
+				// 	eclaboussure1.cmp++;
+				// }, 50);
+
+				// let eclaboussure2 = document.createElement('a-box');
+				// eclaboussure2.setAttribute('width', 1);
+				// eclaboussure2.setAttribute('height', 1);
+				// eclaboussure2.setAttribute('depth', 1);
+				// eclaboussure2.setAttribute('position', position2);
+				// this.el.appendChild(eclaboussure2);
+				// eclaboussure2.cmp = 0;
+				// eclaboussure2.interval = setInterval(() => {
+				// 	eclaboussure2.cmp++;
+				// 	position2.x -= 0.1;
+				// 	if (eclaboussure3.cmp <= 4) {
+				// 		position2.y += 0.1;
+				// 	} else {
+				// 		position2.y -= 0.1;
+				// 	}
+				// 	position2.z += 0.1;
+				//
+				// 	eclaboussure2.setAttribute('position', position2);
+				//
+				// 	if (eclaboussure3.cmp === 8) {
+				// 		clearInterval(eclaboussure3.interval);
+				// 		this.el.removeChild(eclaboussure3);
+				// 	}
+				// }, 50);
+				//
+				// let eclaboussure3 = document.createElement('a-box');
+				// eclaboussure3.setAttribute('width', 1);
+				// eclaboussure3.setAttribute('height', 1);
+				// eclaboussure3.setAttribute('depth', 1);
+				// eclaboussure3.setAttribute('position', position3);
+				// this.el.appendChild(eclaboussure3);
+				// eclaboussure3.cmp = 0;
+				// eclaboussure3.interval = setInterval(() => {
+				// 	eclaboussure3.cmp++;
+				// 	position3.x -= 0.1;
+				// 	if (eclaboussure3.cmp <= 4) {
+				// 		position3.y += 0.1;
+				// 	} else {
+				// 		position3.y -= 0.1;
+				// 	}
+				// 	position3.z += 0.1;
+				//
+				// 	eclaboussure3.setAttribute('position', position3);
+				//
+				// 	if (eclaboussure3.cmp === 8) {
+				// 		clearInterval(eclaboussure3.interval);
+				// 		this.el.removeChild(eclaboussure3);
+				// 	}
+				// }, 50);
+				//
+				// let eclaboussure4 = document.createElement('a-box');
+				// eclaboussure4.setAttribute('width', 1);
+				// eclaboussure4.setAttribute('height', 1);
+				// eclaboussure4.setAttribute('depth', 1);
+				// eclaboussure4.setAttribute('position', new_position);
+				// this.el.appendChild(eclaboussure4);
+				// eclaboussure4.cmp = 0;
+				// eclaboussure4.interval = setInterval(() => {
+				// 	eclaboussure4.cmp++;
+				// 	position4.x -= 0.1;
+				// 	if (eclaboussure4.cmp <= 4) {
+				// 		position4.y += 0.1;
+				// 	} else {
+				// 		position4.y -= 0.1;
+				// 	}
+				// 	position4.z += 0.1;
+				//
+				// 	eclaboussure4.setAttribute('position', position4);
+				//
+				// 	if (eclaboussure3.cmp === 8) {
+				// 		clearInterval(eclaboussure3.interval);
+				// 		this.el.removeChild(eclaboussure3);
+				// 	}
+				// }, 50);
 			}
 			goutte.setAttribute('position', new_position);
 		}, 50);
+
 		this.el.appendChild(goutte);
 	},
 
@@ -60,7 +178,7 @@ AFRAME.registerComponent('cloud', {
 		switch (this.data.type) {
 			case 'raining':
 				this.createRain();
-				setInterval(() => this.createRain(), 100);
+				// setInterval(() => this.createRain(), 100);
 				break;
 		}
 	}
